220909f3e52ae06074d6f303609826915b7a1916
ubeloaf <136755601+ubeloaf@users.noreply.github.com>
2023-06-23 23:25:39 +0000

    add solution

--- main.py
+++ main.py
@@ -1,16 +1,8 @@
 def main():
-    """
-    ##################################################
-    # Comlete your code here
-    Use the same variables: celcius fahrenheit 
-    ##################################################
-    """
+    
+    celcius = float(input('Enter celcius value:\n'))
+    fahrenheit = ((9 / 5) * celcius) + 32
 
-    """
-    ########################################
-    # Do not delete the return statement
-    ########################################
-    """
     return celcius, fahrenheit
 
 
