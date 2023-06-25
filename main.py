06f89c51aa8102c0c12b18e86657b01b05034b93
ubeloaf <136755601+ubeloaf@users.noreply.github.com>
2023-06-25 05:14:12 +0000

    add print statement

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ def main():
     celcius = float(input('Enter celcius value:\n'))
     fahrenheit = ((9 / 5) * celcius) + 32
 
+    print(f'Fahrenheit: {fahrenheit}')
+
     return celcius, fahrenheit
 
 
